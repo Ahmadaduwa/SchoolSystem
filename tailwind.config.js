9c6de1466db95a87bfcf7a0748d5ed6576f1598d
pae_adwaa <adu27747g@gmail.com>
2025-01-25 21:03:18 +0700

    tailwind success

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Views/**/*.cshtml',
+        './wwwroot/**/*.cshtml',
+        './Views/Shared/**/*.cshtml',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
